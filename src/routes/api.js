8be12563ccb76f508f0511ae3e9357d94d5de4b9
trung123jjj <23521677@gm.uit.edu.vn>
2025-11-23 21:57:29 +0700

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,6 +4,10 @@ import table from './api/table.js'
 import room from './api/room.js'
 import locker from './api/locker.js'
 import errorReport from './api/errorReport.js'
+import auth from './api/auth.js'
+import logout from './api/logout.js'
+import refresh from './api/refresh.js'
+import signup from './api/signup.js'
 
 const router = express.Router()
 
@@ -12,5 +16,9 @@ router.use('/tables', table)
 router.use('/rooms', room)
 router.use('/lockers', locker)
 router.use('/error-reports', errorReport)
+router.use('/auth', auth)
+router.use('/logout', logout)
+router.use('/refresh', refresh)
+router.use('/signup', signup)
 export default router
 // module.exports = router
